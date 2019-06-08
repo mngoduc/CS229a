144aa9d4240d837798636bbdd348af8c7cf5ca2f
Minh Ngo Duc <mngoduc@stanford.edu>
2019-06-07 20:04:59 -0700

    lol somehow 0.15 loss lets fukin go

--- 3dprinter/auto_encoder_testing.m
+++ 3dprinter/auto_encoder_testing.m
@@ -3,13 +3,15 @@
 close all; clear all; clc;
 
 load('train_data.mat');
+load('cv_data.mat');
 train.X = train_data.inputs; 
 
 train.y = train_data.UTS;
 train.m = size(train.X, 1);
+layer_size = 7;
+train.autoenc = trainAutoencoder(train.X', layer_size);
 
-train.autoenc = trainAutoencoder(train.X', 4);
-
+cv.X = cv_data.inputs; 
 
 %% 
 train.X_recon = predict(train.autoenc,train.X');
@@ -19,18 +21,34 @@ mseError = mse(train.X'-train.X_recon)
 train.Encoder_b = train.autoenc.EncoderBiases;
 train.Encoder_W = train.autoenc.EncoderWeights;
 
-train.Encoded_X = train.Encoder_W * train.X' + train.Encoder_b
+train.Encoded_X = train.Encoder_W * train.X' + train.Encoder_b;
+cv.Encoded_X = train.Encoder_W * cv.X' + train.Encoder_b;
 %%
 
-theta_init = zeros(4, 1);
-num_iters = 700; 
+theta_init = zeros(layer_size, 1);
+num_iters = 100; 
 alpha = 7.5e-3; 
 lambda = 15; 
 
-[train.theta, train.J_history] = regGradDescent(train.Encoded_X, ...
+[train.theta, train.J_history] = regGradDescent(train.Encoded_X', ...
                                      train_data.UTS,...
                                      theta_init, alpha, ...
                                      lambda, num_iters);
 
-%%
+cv.J = zeros(num_iters, 1);
+for i = 1:length(train.theta)
+    cv.J(i) = computeCostMulti(cv.Encoded_X', ...
+                               cv_data.UTS, ...
+                               train.theta(:,i));
+end 
+
+figure;
 plot(train.J_history)
+hold on; 
+plot(cv.J)
+legend("training error", "cross-validation")
+title("Using all available features")
+xlabel('Iterations')
+ylabel('Cost')
+plotfixer;
+savefig("Cost_auto_encoder.fig")
